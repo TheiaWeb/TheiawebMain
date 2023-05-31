f31f2ff118788bd15b547d266f6918cd28824b8b
Pauline Mila Alonso <113476814+Milhaal@users.noreply.github.com>
2023-06-01 00:37:26 +0200

    déroulant expertises

--- js/script.js
+++ js/script.js
@@ -9,15 +9,32 @@ function toggleMenu() {
 
   if (!menuOpen) {
     menu.classList.add('menu-open');
-    icon.src = '../img/navbar-close.png';
+    icon.src = 'img/navbar-close.png';
     menuOpen = true;
   } else {
     menu.classList.remove('menu-open');
-    icon.src = '../img/navbar-burger.png';
+    icon.src = 'img/navbar-burger.png';
     menuOpen = false;
   }
 }
 
+
+/*==================== TOGGLE TABS ====================*/
+let tabsPortfolio = document.querySelectorAll('.tabs__toggle'),
+    contents = document.querySelectorAll('.tabs__content');
+
+    tabsPortfolio.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        contents.forEach((content) => {
+            content.classList.remove('is-active');
+        });
+        tabsPortfolio.forEach((tab) => {
+            tab.classList.remove('is-active');
+        });
+        contents[index].classList.add('is-active');
+        tabsPortfolio[index].classList.add('is-active');
+    });
+});
 
 
 
