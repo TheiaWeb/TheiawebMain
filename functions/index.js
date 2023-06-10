ec1dd8e840f8e5a6f06dbfde809522f530e51c2b
Haxxxxxx <62444678+Haxxxxxx@users.noreply.github.com>
2023-06-10 13:25:48 +0200

    IMPLEMENTATION CODE THEIA MAIN

---
+++ functions/index.js
@@ -0,0 +1,49 @@
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+const cors = require('cors')({ origin: true });
+
+exports.sendEmailToUser = functions.firestore
+  .document('contacts/{contactId}')
+  .onCreate((snapshot, context) => {
+    const contactData = snapshot.data();
+
+    // Configure the email transport
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'theiaweb.contact@gmail.com',
+        pass: 'cwsmnseyqepnqplf'
+      }
+    });
+
+    const mailOptions = {
+      from: 'theiaweb.contact@gmail.com',
+      to: contactData.email,
+      subject: 'Merci pour votre prise de contact',
+      text: `Chèr(e) ${contactData.name},\n\nMerci de nous avoir contactés. Nous avons bien reçu votre message et nous vous répondrons dans les plus brefs délais.\n\nCordialement,\nTheia Web`,
+      html: `<p>Chèr(e) ${contactData.name},</p>
+        <p>Merci de nous avoir contactés. Nous avons bien reçu votre message et nous vous répondrons dans les plus brefs délais.</p>
+        <p>Cordialement,<br>Theia Web</p>`
+    };
+
+    const mailAdmin = {
+      from: 'theiaweb.contact@gmail.com',
+      to: 'theiaweb.contact@gmail.com',
+      subject: 'Info client',
+      text: `Info \n\n Nom du client : ${contactData.surname} ${contactData.name},\n\n Email : ${contactData.email}\n\n Téléphone : ${contactData.phone}\n\n Compagny/Entreprise du client : ${contactData.company} \n\n Sujet du message : ${contactData.subject} \n\n Contenu du message envoyé : ${contactData.message}`,
+      html: `<p> Info <br><br> Nom du client : ${contactData.surname} ${contactData.name},<br><br>Email : ${contactData.email}<br><br>Téléphone : ${contactData.phone}<br><br> Compagny/Entreprise du client : ${contactData.company} <br><br> Sujet du message : ${contactData.subject} <br><br> Contenu du message envoyé : ${contactData.message}</p>`
+    };
+
+    const sendMailToUser = transporter.sendMail(mailOptions);
+    const sendMailToAdmin = transporter.sendMail(mailAdmin);
+
+    return Promise.all([sendMailToUser, sendMailToAdmin])
+      .then(() => {
+        console.log('Emails sent successfully');
+        return null;
+      })
+      .catch((error) => {
+        console.error('Error sending emails:', error);
+        return null;
+      });
+  });
---
+++ public/js/script.js
@@ -0,0 +1,40 @@
+
+// -----------------TOGGLE MENU------------------- //
+
+var menuOpen = false;
+
+function toggleMenu() {
+  var menu = document.getElementById('menu');
+  var icon = document.getElementById('iconImage');
+
+  if (!menuOpen) {
+    menu.classList.add('menu-open');
+    icon.src = 'img/navbar-close.png';
+    menuOpen = true;
+  } else {
+    menu.classList.remove('menu-open');
+    icon.src = 'img/navbar-burger.png';
+    menuOpen = false;
+  }
+}
+
+
+/*==================== TOGGLE TABS ====================*/
+let tabsPortfolio = document.querySelectorAll('.tabs__toggle'),
+    contents = document.querySelectorAll('.tabs__content');
+
+    tabsPortfolio.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        contents.forEach((content) => {
+            content.classList.remove('is-active');
+        });
+        tabsPortfolio.forEach((tab) => {
+            tab.classList.remove('is-active');
+        });
+        contents[index].classList.add('is-active');
+        tabsPortfolio[index].classList.add('is-active');
+    });
+});
+
+
+
