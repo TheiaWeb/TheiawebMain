9cc0f86aac66b6de136427c1e6cf4374f89a0d6f
Pauline Mila Alonso <113476814+Milhaal@users.noreply.github.com>
2023-06-19 23:38:03 +0200

    push team-rs, new font, contact + top btn

--- private/js/script.js
+++ private/js/script.js
@@ -35,6 +35,27 @@ let tabsPortfolio = document.querySelectorAll('.tabs__toggle'),
     });
 });
 
+/*==================== GO TOP BTN ====================*/
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 /*==================== TEST ANIMATION ====================*/
 
 // wow = new WOW(
