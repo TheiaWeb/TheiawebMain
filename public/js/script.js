ad0aec8f750f9dc663dfb514d078f7d9a90c8921
Haxxxxxx <62444678+Haxxxxxx@users.noreply.github.com>
2023-06-23 15:33:02 +0200

    [MEP V0.4] - 23/06/2023
    [FRO]  :
    - Nouvelle font déployé sur index.html
    - Ajout du bouton "goTop"
    - Ajout des réseaux sociaux dans la section <team>
    - modification du box-shadow du <contact form>
    
    [BCK]
    - Rectification sur le popUp RGPD en local -  ✅  -> Essaie d'import dans un serveur local ✅
    - Allegement du code html en linkan toutes les methodes JS dans un file annexe 🛑
    - Mise en place structure e-commerce 🛑
    - Implementation API twitch (reste le twitch developpeur side a faire) ✅
    - Newsletter hotfix des bugs de formulaire / inscription (reste en private jusqu'a besoin) ✅
    - Generateur de citations aléatoire ✅

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,10 @@
 // -----------------TOGGLE MENU------------------- //
 
-var menuOpen = false;
+let menuOpen = false;
 
 function toggleMenu() {
-  var menu = document.getElementById('menu');
-  var icon = document.getElementById('iconImage');
+  const menu = document.getElementById('menu');
+  const icon = document.getElementById('iconImage');
 
   if (!menuOpen) {
     menu.classList.add('menu-open');
@@ -19,22 +19,41 @@ function toggleMenu() {
 
 
 /*==================== TOGGLE TABS ====================*/
-let tabsPortfolio = document.querySelectorAll('.tabs__toggle'),
-    contents = document.querySelectorAll('.tabs__content');
+const tabsPortfolio = document.querySelectorAll('.tabs__toggle');
+const contents = document.querySelectorAll('.tabs__content');
 
-    tabsPortfolio.forEach((tab, index) => {
-    tab.addEventListener('click', () => {
-        contents.forEach((content) => {
-            content.classList.remove('is-active');
-        });
-        tabsPortfolio.forEach((tab) => {
-            tab.classList.remove('is-active');
-        });
-        contents[index].classList.add('is-active');
-        tabsPortfolio[index].classList.add('is-active');
+tabsPortfolio.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    contents.forEach((content) => {
+      content.classList.remove('is-active');
     });
+    tabsPortfolio.forEach((tab) => {
+      tab.classList.remove('is-active');
+    });
+    contents[index].classList.add('is-active');
+    tabsPortfolio[index].classList.add('is-active');
+  });
 });
+/*==================== GO TOP BTN ====================*/
+// Go Top Button
+const mybutton = document.getElementById('myBtn');
 
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 /*==================== TEST ANIMATION ====================*/
 
 // wow = new WOW(
@@ -49,35 +68,156 @@ let tabsPortfolio = document.querySelectorAll('.tabs__toggle'),
 // )
 // wow.init();
 
-     /*==================== PopUp RGPD====================*/       
 
-     document.addEventListener("DOMContentLoaded", function() {
-      var popupRGPD = document.getElementById("popupRGPD");
-      var acceptButton = document.getElementById("acceptButton");
-  
-      acceptButton.addEventListener("click", function() {
-      popupRGPD.style.display = "none";
-      popupRGPD.style.position = "null";
-      popupRGPD.style.opacity = "100%";
-      });
-    });
+/*==================== PopUp CONTACT FORM====================*/
 
-/*==================== PopUp CONTACT FORM====================*/       
-      function closePopup() {
-    var popupFORM = document.getElementById('popupFORM');
-    popupFORM.style.display = 'none';
-  }
+function closePopup() {
+  const popup = document.getElementById('popupFORM');
+  popup.style.display = 'none';
+}
 
-  function openPopup() {
-    var popupFORM = document.getElementById('popupFORM');
-    popupFORM.style.display = 'block';
-  }
+document.addEventListener('DOMContentLoaded', function () {
+  const submitBtn = document.querySelector('.green_btn');
 
-  var submitBtn = document.querySelector('.green_btn');
-  submitBtn.addEventListener('click', function(event) {
+  submitBtn.addEventListener('click', function (event) {
     event.preventDefault();
     console.log("Form submitted successfully!");
-    openPopup();
+    const popup = document.getElementById('popupFORM');
+    popup.style.display = 'block';
   });
 
- 
+  const closeBtn = document.querySelector('.close');
+  closeBtn.addEventListener('click', function () {
+    closePopup();
+  });
+});
+
+/*==================== PopUp RGPD====================*/
+document.addEventListener('DOMContentLoaded', function () {
+  const popup = document.getElementById('popupRGPD');
+  const acceptButton = document.getElementById('acceptButton');
+
+  acceptButton.addEventListener('click', function () {
+    popup.style.display = 'none';
+    localStorage.setItem('consentGiven', true);
+  });
+
+  const consentGiven = localStorage.getItem('consentGiven');
+
+  if (!consentGiven) {
+    centerPopup();
+  }
+
+  function centerPopup() {
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+    const popupHeight = popup.offsetHeight;
+    const popupWidth = popup.offsetWidth;
+
+    const topOffset = (windowHeight - popupHeight) / 2;
+    const leftOffset = (windowWidth - popupWidth) / 2;
+
+    popup.style.top = topOffset + 'px';
+    popup.style.left = leftOffset + 'px';
+  }
+
+  window.addEventListener('resize', centerPopup);  
+});
+
+//===========Generateur de Citations (PAGE 404)==========//
+const pcCitations = [
+  "Les ordinateurs sont comme des dieux de l'Ancien Testament ; plein de règles et sans pitié.",
+  "L'ordinateur est né pour résoudre des problèmes qui n'existaient pas auparavant.",
+  "N'importe quel idiot peut utiliser un ordinateur. Et beaucoup le font.",
+  "La meilleure chose à propos d'un ordinateur, c'est qu'il est prévisible ; il ne blâme jamais ses erreurs sur vous.",
+  "L'ordinateur est un crétin.",
+];
+
+const iaCitations = [
+  "L'intelligence artificielle ne fait pas le poids face à la stupidité naturelle.",
+  "L'IA est comme une rock star qui joue une musique sans faute, mais qui détruit des chambres d'hôtel.",
+  "L'IA est le nouvel électricité.",
+  "La question n'est pas de savoir si l'IA dépassera l'intelligence humaine, mais quand et comment.",
+  "L'IA n'est pas une baguette magique ; c'est un outil pour augmenter les capacités humaines.",
+];
+
+const webCitations = [
+  "Le web tel que je l'ai envisagé, nous ne l'avons pas encore vu. L'avenir est encore bien plus grand que le passé.",
+  "La bonne nouvelle à propos des ordinateurs, c'est qu'ils font ce que vous leur dites de faire. La mauvaise nouvelle, c'est qu'ils font ce que vous leur dites de faire.",
+  "Le web est davantage une création sociale qu'une création technique. Je l'ai conçu pour un effet social, pour aider les gens à travailler ensemble, et non comme un jouet technique.",
+  "Internet pourrait être une étape très positive vers l'éducation, l'organisation et la participation à une société significative.",
+  "Internet devient la place du village global de demain.",
+];
+
+function getRandomCitation(citations) {
+  const randomIndex = Math.floor(Math.random() * citations.length);
+  return citations[randomIndex];
+}
+
+function displayRandomCitation() {
+  const citationElement = document.getElementById("citation");
+  const randomCitation = getRandomCitation([...pcCitations, ...iaCitations, ...webCitations]);
+  citationElement.textContent = randomCitation;
+}
+
+// Affiche une citation aléatoire lorsque la page se charge ou se rafraîchit
+displayRandomCitation();
+
+
+// //===========NEWSLETTER SUBSCRIPTION==========//
+//   // Handle newsletter subscription
+//   document.getElementById('newsletterForm').addEventListener('submit', function(event) {
+//     event.preventDefault(); // Prevent form from submitting and page refresh
+
+//     // Get the email input value
+//     var email = document.getElementById('newsletterEmail').value;
+
+//     // Save the email to the Firestore database
+//     db.collection('subscribers').add({
+//       email: email,
+//       timestamp: firebase.firestore.FieldValue.serverTimestamp()
+//     })
+//     .then(function(docRef) {
+//       console.log('Newsletter subscription added successfully!');
+//       // Reset email input field
+//       document.getElementById('newsletterEmail').value = '';
+//     })
+//     .catch(function(error) {
+//       console.error('Error adding newsletter subscription:', error);
+//     });
+//   });
+
+
+//===========TWITCH API==========//
+
+// function fetchAndDisplayVideos() {
+//   //YOUR_USER_ID == l'id du compte twitch 
+//   fetch('https://api.twitch.tv/helix/videos?user_id=YOUR_USER_ID', {
+//     headers: {
+//       // Client ID == Id a recupere via le portail de developpeur de twitch
+//       'Client-ID': 'YOUR_CLIENT_ID',
+//       // Access token == required for authentification 
+//       // Pas nescessaire dans tout les cas 
+//       'Authorization': 'Bearer YOUR_ACCESS_TOKEN' 
+//     }
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       const videos = data.data; // Array of video objects
+//       const embedCodes = videos.map(video => `
+//         <iframe
+//           src="https://player.twitch.tv/?video=${video.id}"
+//           height="480"
+//           width="720"
+//           allowfullscreen="true">
+//         </iframe>
+//       `);
+//       const videoContainer = document.getElementById('video-container');
+//       videoContainer.innerHTML = embedCodes.join('');
+//     })
+//     .catch(error => {
+//       console.error('Error retrieving video data:', error);
+//     });
+// }
+
+// fetchAndDisplayVideos();
