1798f224c6242902dfd25339cf9d0d8432608cd3
Haxxxxxx <62444678+Haxxxxxx@users.noreply.github.com>
2023-08-11 11:20:44 +0200

    oopsy eheh

--- public/js/script.js
+++ public/js/script.js
@@ -281,15 +281,42 @@ acceptButtonMultiple.addEventListener('click', async () => {
 
 //#region Player Twitch
 const clientId = 'o5x6ltrt4jwhb6ybxm0kkpjcip5mk4';
-const accessToken = 'vqyqxbi9o0j34m19drzgvgzypk849w';
+const accessToken = 'krnc4iwqx3gl1e8c9s5d6fkamjs7ig';
 const channelId = 'theiaweb';
 
 var playerWidth = 1472;
 var playerHeight = 545; // Adjusted height to maintain 16:9 aspect ratio (16/9 = 1.7778)
 var twitchPlayer = null; // Variable to store the Twitch player instance
-const nextScheduledStreamChannelId = 'next_channel_id'; // Replace with the channel ID of the channel with the next scheduled stream
-let nextStreamDateValue = "Date: Not available";
+// const nextScheduledStreamChannelId = 'next_channel_id'; // Replace with the channel ID of the channel with the next scheduled stream
+// let nextStreamDateValue = "Date: Not available";
 
+// Call the function on page load
+window.onload = function () {
+  initializePlayer();
+  fetch(`https://api.twitch.tv/helix/subscriptions?broadcaster_id=${channelId}`, {
+    headers: {
+      'Client-ID': clientId,
+      'Authorization': `Bearer ${accessToken}`
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Process the data here
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Fetch error:', error);
+  });
+  
+};
+
+// Call the function whenever the window is resized
+window.addEventListener('resize', initializePlayer);
 
 function initializePlayer() {
   if (twitchPlayer) {
@@ -321,93 +348,81 @@ function initializePlayer() {
   });
 
   // Fetch live stream information from the Twitch API
-  fetchLiveStreamInfo();
+  // fetchLiveStreamInfo();
 
 }
 
 
-function fetchNextScheduledStream() {
-  fetch(`https://api.twitch.tv/helix/schedule?broadcaster_id=${nextScheduledStreamChannelId}`, {
-    method: 'GET',
-    headers: {
-      'Client-ID': clientId,
-      'Authorization': `Bearer ${accessToken}`
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    if (data.data && data.data.length > 0) {
-      // Next scheduled stream is available, update the information
-      var nextStreamTitle = data.data[0].title;
-      nextStreamDateValue = data.data[0].scheduled_start_time; // Update the global variable
+// function fetchNextScheduledStream() {
+//   fetch(`https://api.twitch.tv/helix/schedule?broadcaster_id=${nextScheduledStreamChannelId}`, {
+//     method: 'GET',
+//     headers: {
+//       'Client-ID': clientId,
+//       'Authorization': `Bearer ${accessToken}`
+//     }
+//   })
+//   .then(response => response.json())
+//   .then(data => {
+//     if (data.data && data.data.length > 0) {
+//       // Next scheduled stream is available, update the information
+//       var nextStreamTitle = data.data[0].title;
+//       nextStreamDateValue = data.data[0].scheduled_start_time; // Update the global variable
 
-      document.getElementById('nextStreamTitle').textContent = nextStreamTitle;
-      document.getElementById('nextStreamDate').textContent = `Aucun stream de prevu donc la Date: ${nextStreamDateValue}`;
-      document.getElementById('nextStreamInfo').style.display = 'block';
-    } else {
-      // No next scheduled stream, hide the information
-      document.getElementById('nextStreamInfo').style.display = 'block'; //A modifier une fois les streams lancer
-    }
-  })
-  .catch(error => {
-    console.error('Error fetching next scheduled stream information:', error);
-  });
-}
+//       document.getElementById('nextStreamTitle').textContent = nextStreamTitle;
+//       document.getElementById('nextStreamDate').textContent = `Aucun stream de prevu donc la Date: ${nextStreamDateValue}`;
+//       document.getElementById('nextStreamInfo').style.display = 'block';
+//     } else {
+//       // No next scheduled stream, hide the information
+//       document.getElementById('nextStreamInfo').style.display = 'block'; //A modifier une fois les streams lancer
+//     }
+//   })
+//   .catch(error => {
+//     console.error('Error fetching next scheduled stream information:', error);
+//   });
+// }
 
-function fetchLiveStreamInfo() {
-  fetch(`https://api.twitch.tv/helix/streams?user_login=${channelId}`, {
-    method: 'GET',
-    headers: {
-      'Client-ID': clientId,
-      'Authorization': `Bearer ${accessToken}`
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    if (data.data && data.data.length > 0) {
-      // Stream is live, update the live information
-      var streamTitle = data.data[0].title;
-      document.getElementById('streamTitle').textContent = streamTitle;
+// function fetchLiveStreamInfo() {
+//   fetch(`https://api.twitch.tv/helix/streams?user_login=${channelId}`, {
+//     method: 'GET',
+//     headers: {
+//       'Client-ID': clientId,
+//       'Authorization': `Bearer ${accessToken}`
+//     }
+//   })
+//   .then(response => response.json())
+//   .then(data => {
+//     if (data.data && data.data.length > 0) {
+//       // Stream is live, update the live information
+//       var streamTitle = data.data[0].title;
+//       document.getElementById('streamTitle').textContent = streamTitle;
 
-    } else {
-      // Stream is offline, display default information
-      document.getElementById('streamTitle').textContent = 'Hors Ligne Revenez demain !';
-    }
-  })
-  .catch(error => {
-    console.error('Error fetching live stream information:', error);
-  });
-}
-
-// Call the function on page load
-window.onload = function () {
-  initializePlayer();
-  fetch(`https://api.twitch.tv/helix/subscriptions?broadcaster_id=${channelId}`, {
-    headers: {
-      'Client-ID': clientId,
-      'Authorization': `Bearer ${accessToken}`
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      const subscriberCount = data.total;
+//     } else {
+//       // Stream is offline, display default information
+//       document.getElementById('streamTitle').textContent = 'Hors Ligne Revenez demain !';
+//     }
+//   })
+//   .catch(error => {
+//     console.error('Error fetching live stream information:', error);
+//   });
+// }
 
 
-      if (subscriberCount === undefined ){
-        document.getElementById('subscriberCount').innerText = "Rip aucun abonnés";
+    // .then(response => response.json())
+    // .then(data => {
+    //   const subscriberCount = data.total;
 
-      }
-      else{
-        document.getElementById('subscriberCount').innerText = subscriberCount;
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching subscriber count:', error);
-    });
-};
 
-// Call the function whenever the window is resized
-window.addEventListener('resize', initializePlayer);
+    //   if (subscriberCount === undefined ){
+    //     document.getElementById('subscriberCount').innerText = "Rip aucun abonnés";
+
+    //   }
+    //   else{
+    //     document.getElementById('subscriberCount').innerText = subscriberCount;
+    //   }
+    // })
+    // .catch(error => {
+    //   console.error('Error fetching subscriber count:', error);
+    // });
 
 
 //#endregion
