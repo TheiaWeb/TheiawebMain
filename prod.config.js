6924fc2c124d0841bc3e057b8e925d9f95707693
Haxxxxxx <vinc.charles0@gmail.com>
2023-09-20 10:44:12 +0200

    Update prod.config.js

--- prod.config.js
+++ prod.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production', // Set the build mode to 'production'
-  entry: 'TheiaWeb/js/script.js', // Entry point of your JavaScript
+  entry: './TheiaWeb/js/script.js', // Entry point of your JavaScript
   output: {
     filename: 'bundle.js', // Name of the output JavaScript bundle file
     path: path.resolve(__dirname, 'dist'), // Output directory
