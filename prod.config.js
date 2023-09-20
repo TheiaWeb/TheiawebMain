d9e88669d68ab888105f12c3c77614c356b66513
Haxxxxxx <vinc.charles0@gmail.com>
2023-09-20 10:37:34 +0200

    test addition bundle to yaml

---
+++ prod.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production', // Set the build mode to 'production'
+  entry: './TheiaWeb/js/index.js', // Entry point of your JavaScript
+  output: {
+    filename: 'bundle.js', // Name of the output JavaScript bundle file
+    path: path.resolve(__dirname, 'dist'), // Output directory
+  },
+};
